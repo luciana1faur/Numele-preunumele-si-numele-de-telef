86870321edba0251062794ff5772600895b8ea11
Faur Luciana <luciana_faur@yahoo.com>
2020-11-14 08:42:57 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ char nume, prenume;
+ int nrTelef;
+ 
+ cout << "Introduceti numele dumneavoastra: ";
+ cin >> nume;
+ cout << "Introduceti prenumele dumneavoastra: ";
+ cin >> prenume;
+ 
+  cout << "Introduceti numarul dumneavoastra de telefon: ";
+ cin >> nrTelef;
+
+ cout << "Numele: " << nume << ", prenume: " << prenume << ", numarul de telefon: " << nrTelef << endl;
+}
